6219128063c5ee1d255938bdab5f99b6d16202d8
Nan Bai <billwisebai@gmail.com>
2023-10-12 19:49:50 +1100

    feat: image layout

---
+++ src/components/ImgLayout/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ImgLayout.css';
+
+const ImgLayout = () => {
+    const imgList = [
+        {"url": "https://images.pexels.com/photos/11376797/pexels-photo-11376797.jpeg"},
+        {"url": "https://images.pexels.com/photos/10433031/pexels-photo-10433031.jpeg"},
+        {"url": "https://images.pexels.com/photos/18607904/pexels-photo-18607904/free-photo-of-vineta-auf-dem-stormthaler-see.png"},
+        {"url": "https://images.pexels.com/photos/17715085/pexels-photo-17715085/free-photo-of-brown-bear.jpeg"},
+        {"url": "https://images.pexels.com/photos/18137869/pexels-photo-18137869/free-photo-of-chinese-pavilion-in-by-pond.jpeg"},
+        {"url": "https://images.pexels.com/photos/8869973/pexels-photo-8869973.jpeg"},
+        {"url": "https://images.pexels.com/photos/10433031/pexels-photo-10433031.jpeg"},
+        {"url": "https://images.pexels.com/photos/18607904/pexels-photo-18607904/free-photo-of-vineta-auf-dem-stormthaler-see.png"},
+        {"url": "https://images.pexels.com/photos/18137869/pexels-photo-18137869/free-photo-of-chinese-pavilion-in-by-pond.jpeg"},
+        {"url": "https://images.pexels.com/photos/8869973/pexels-photo-8869973.jpeg"},
+    ];
+    const imgItems = imgList.map((item) => 
+        <div class="image-box" >
+            <img src={item.url} alt="" />
+        </div>
+    )
+    return(
+        <>
+            <div class='page'>
+                <div class="waterfall-width-column">
+                    {imgItems}
+                </div>
+            </div>
+
+        </>
+
+    )
+}
+
+export default ImgLayout
