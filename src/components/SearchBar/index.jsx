5eefe1593e5ef2aa1080a239fc4a92e19a3ce3d1
Nan Bai <billwisebai@gmail.com>
2023-10-12 19:49:25 +1100

    feat: search bar

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useState } from 'react';
+
+const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [result, setResult] = useState([]);
+
+    // const handleChange = (e) => {
+    //     e.preventDefault();
+    //     console.log(e.target.value);
+    //     setSearchInput(e.target.value);    
+    //     // fetchData(e.target.value)
+    // };
+    let timer;
+    const handleTimeout = useCallback((e) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            // console.log(e.target.value);
+            if (e.target.value) {
+                fetchData(e.target.value);
+            }
+        },1000)
+    }, [])
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+        if (!e.target.value) {
+            setResult([]);
+        }
+        handleTimeout(e);
+    }
+
+    const fetchData = (input) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?q=${input}`)
+        .then(response => response.json())
+        .then(json => setResult(json))
+    }
+
+    return(
+        <>
+            <section>
+            <h1>Search Bar</h1>
+            <input
+                type="text"
+                placeholder="Search here"   
+                onChange={handleChange}
+                value={searchInput}  />
+            </section>
+            <ol>
+                {result.map((value, index) => {
+                    return<li key={index} >{value['title']}</li>
+                })}
+            </ol>
+        </>
+
+    )
+}
+
+export default SearchBar
